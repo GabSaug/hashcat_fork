e3c16a4af02fc0c66887cd4d38d1346de5e339dd
SAUGER Gabriel <gabriel.sauger@loria.fr>
2024-06-17 15:09:46 +0200

    Prepared for benchmark

---
+++ compile_no_configure.sh
@@ -0,0 +1,13 @@
+mkdir -p build/bin
+make clean > /dev/null
+rm build/bin/hashcat
+opt="$(echo $1 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -fno-inline"
+make EXTRA_CFLAGS=" $opt" -j -n &> log_make.txt
+if ! make EXTRA_CFLAGS="$opt" -j ; then
+	echo "error make"
+	exit 1
+fi
+if ! cp hashcat build/bin/hashcat; then
+	echo "error copying binary"
+	exit 1
+fi
