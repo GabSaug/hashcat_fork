84bc6d88c81667b2c2beb077772cf86dacac4e85
SAUGER Gabriel <gabriel.sauger@loria.fr>
2024-06-19 21:11:05 +0200

    updateed

--- compile_to_asm.sh
+++ compile_to_asm.sh
@@ -2,7 +2,7 @@
 # $2 output path
 opt="$(echo $3 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -fno-inline"
 
-if ! gcc -pipe -Iinclude/ -IOpenCL/ -Ideps/LZMA-SDK/C -Ideps/zlib -Ideps/zlib/contrib -Ideps/OpenCL-Headers -Ideps/xxHash -DWITH_CUBIN -Ideps/unrar $opt "$1" -o "$2" -lpthread -ldl -lrt -lm -shared -fPIC -D  MODULE_INTERFACE_VERSION_CURRENT=700; then
+if ! gcc -S -pipe -Iinclude/ -IOpenCL/ -Ideps/LZMA-SDK/C -Ideps/zlib -Ideps/zlib/contrib -Ideps/OpenCL-Headers -Ideps/xxHash -DWITH_CUBIN -Ideps/unrar $opt "$1" -o "$2" -lpthread -ldl -lrt -lm -shared -fPIC -D  MODULE_INTERFACE_VERSION_CURRENT=700; then
 	echo "error compile to asm"
 	exit 1
 fi
