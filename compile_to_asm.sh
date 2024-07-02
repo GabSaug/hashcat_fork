4205f5cb3eb7ad2f0c005d82454d89c6171eb9ee
SAUGER Gabriel <gabriel.sauger@loria.fr>
2024-07-02 21:22:05 +0200

    crorect noinline

--- compile_to_asm.sh
+++ compile_to_asm.sh
@@ -1,6 +1,6 @@
 # $1: name of the c file to compile to assembly
 # $2 output path
-opt="$(echo $3 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -fno-inline"
+opt="$(echo $3 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -finline-limit=2"
 
 if ! gcc -S -masm=intel -pipe -Iinclude/ -IOpenCL/ -Ideps/LZMA-SDK/C -Ideps/zlib -Ideps/zlib/contrib -Ideps/OpenCL-Headers -Ideps/xxHash -DWITH_CUBIN -Ideps/unrar $opt "$1" -o "$2" -lpthread -ldl -lrt -lm -shared -fPIC -D  MODULE_INTERFACE_VERSION_CURRENT=700; then
 	echo "error compile to asm"
